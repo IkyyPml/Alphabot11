fd565d2a13455a510ad5587c9b3994c505a7a492
IkyyPml <103631655+IkyyPml@users.noreply.github.com>
2022-04-21 22:25:03 +0700

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -2,14 +2,14 @@
 const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
-1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
-2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
-3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
-4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
+1. *IkyyBotz Support*
+_-_
+2. *IkyyBotz Support 2*
+_-_
+3. *IkyyBotz Support 3*
+_-_
+4. *IkyyBotz Support 4* 
+_-_
 
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
